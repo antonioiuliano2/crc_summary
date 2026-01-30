63d92c7512a868682b86a402221cf0cbc2c32315
Antonio Iuliano <antonio.iuliano@na.infn.it>
2026-01-30 12:13:35 +0100

    adding new school

--- addtelescope.py
+++ addtelescope.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 
-newport = "7762"
+newport = "7763"
 
-dftot = pd.read_csv("/home/antonio/OCRA/CRC_monitoring/crc_summary/CRCNetwork_status_report.csv", index_col="Time")
+dftot = pd.read_csv("/home/iuliano/CRC_monitoring/crc_summary/CRCNetwork_status_report.csv", index_col="Time")
 dftot[newport] = 0
 
-dftot.to_csv("/home/antonio/OCRA/CRC_monitoring/crc_summary/CRCNetwork_status_report_updated.csv")
+dftot.to_csv("/home/iuliano/CRC_monitoring/crc_summary/CRCNetwork_status_report_updated.csv")
 
