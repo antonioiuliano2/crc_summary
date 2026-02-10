fa61a7bb83394d2538b03357beac135b7e93c6f1
Antonio Iuliano <antonio.iuliano@na.infn.it>
2026-02-10 11:00:07 +0100

    adding new school with CRC

--- addtelescope.py
+++ addtelescope.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-newport = "7763"
+newport = "7764"
 
 dftot = pd.read_csv("/home/iuliano/CRC_monitoring/crc_summary/CRCNetwork_status_report.csv", index_col="Time")
 dftot[newport] = 0
