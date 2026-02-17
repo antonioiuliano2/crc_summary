a7832ddd38a32ea42b1e8781e56db8cbc7994bef
Antonio Iuliano <antonio.iuliano@na.infn.it>
2026-02-17 17:40:32 +0100

    adding liceo Galileo Galilei

--- addtelescope.py
+++ addtelescope.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-newport = "7764"
+newport = "7765"
 
 dftot = pd.read_csv("/home/iuliano/CRC_monitoring/crc_summary/CRCNetwork_status_report.csv", index_col="Time")
 dftot[newport] = 0
